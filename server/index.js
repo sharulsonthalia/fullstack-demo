f56ebf8c942b45641838f01f2d5f48ef45048398
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-08-17 12:04:25 -0700

    initial server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3005;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1> Choo Choo!');
+});
+
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server is up and running and listening on port ${PORT}`);
+  } else {
+    console.log(`Something went wrong`);
+  }
+});
