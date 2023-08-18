aaef15e7f0df4a3479c6eec3447948241511ade9
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-08-18 13:03:54 -0700

    Authorization with jwt

--- server/auth/index.js
+++ server/auth/index.js
@@ -20,9 +20,9 @@ router.post("/register", async (req, res) => {
     });
 
     if (result) {
-      const token = jwt.sign({ id: result.id}, process.env.JWT); 
+      const token = jwt.sign({ id: result.id }, process.env.JWT);
 
-      res.status(201).send({token});
+      res.status(201).send({ token });
     } else {
       res.send({ message: "Could not add User" });
     }
@@ -42,13 +42,33 @@ router.post("/signIn", async (req, res) => {
   if (user) {
     const passwordMatch = await bcrypt.compare(password, user.password); //<===Will return true if password is correct
     if (passwordMatch) {
-      //TODO return jwt token
-      res.send(user);
+      const token = jwt.sign({ id: user.id }, process.env.JWT);
+
+      res.send({ token });
     } else {
       res.send({ message: "Invalid Login" });
     }
   } else {
     res.send({ message: "Invalid Login" });
+  }
+});
+
+//Route that sends the user based on the given token
+router.get("/me", async (req, res) => {
+  //Checks the request for the userId which 
+  //was set by our middleware in server/index.js
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.userId },
+    });
+
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ message: "User Not Found" });
+    }
+  } catch (error) {
+    res.send(error.message);
   }
 });
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 3005;
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //Middleware to format body into JSON
 app.use(require("body-parser").json());
 
 //Middleware for logging
-app.use(require("morgan")('dev'));
+app.use(require("morgan")("dev"));
+
+app.use((req, res, next) => {
+  //Check if the authorization header is valid
+  const auth = req.headers.authorization;
+  const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
+  try{
+    const { id } = jwt.verify(token, process.env.JWT);
+    req.userId = id;
+  }
+  catch{
+    req.userId = null;
+  }
+
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1> Choo Choo!");
